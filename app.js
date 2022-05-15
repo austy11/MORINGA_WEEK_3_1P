a6d96238589efc7641d2729816f920bb0d52e4db
steve <stephen.lukanu@student.moringaschool.com>
2022-05-15 15:50:45 +0300

    added javascript code to define button behaviour

---
+++ app.js
@@ -0,0 +1,112 @@
+// --------- qst 1 ---------
+// next button
+document.getElementById('nxtbut1').onclick = function(){
+    document.getElementById('questionArea2').style.display = "block"; 
+    document.getElementById('questionArea1').style.display = "none"; 
+}
+
+// --------- qst 2 ---------
+// next btn
+document.getElementById('nxtbut2').onclick = function(){
+    document.getElementById('questionArea3').style.display = "block"; 
+    document.getElementById('questionArea2').style.display = "none";
+}
+// prev btn
+document.getElementById('prevbut2').onclick = function(){
+    document.getElementById('questionArea1').style.display = "block";
+    document.getElementById('questionArea2').style.display = "none";
+}
+
+// --------- qst 3 ---------
+// next btn
+document.getElementById('nxtbut3').onclick = function(){
+    document.getElementById('questionArea4').style.display = "block"; 
+    document.getElementById('questionArea3').style.display = "none";
+}
+// prev btn
+document.getElementById('prevbut3').onclick = function(){
+    document.getElementById('questionArea2').style.display = "block";
+    document.getElementById('questionArea3').style.display = "none";
+}
+
+// --------- qst 4 ---------
+// next btn
+document.getElementById('nxtbut4').onclick = function(){
+    document.getElementById('questionArea5').style.display = "block"; 
+    document.getElementById('questionArea4').style.display = "none";
+}
+// prev btn
+document.getElementById('prevbut4').onclick = function(){
+    document.getElementById('questionArea3').style.display = "block";
+    document.getElementById('questionArea4').style.display = "none";
+}
+
+// --------- qst 5 ---------
+// next btn
+document.getElementById('nxtbut5').onclick = function(){
+    document.getElementById('questionArea6').style.display = "block"; 
+    document.getElementById('questionArea5').style.display = "none";
+}
+// prev btn
+document.getElementById('prevbut5').onclick = function(){
+    document.getElementById('questionArea4').style.display = "block";
+    document.getElementById('questionArea5').style.display = "none";
+}
+
+// --------- qst  6---------
+// next button
+document.getElementById('nxtbut6').onclick = function(){
+    document.getElementById('questionArea7').style.display = "block"; 
+    document.getElementById('questionArea6').style.display = "none"; 
+}
+// prev btn
+document.getElementById('prevbut6').onclick = function(){
+    document.getElementById('questionArea5').style.display = "block";
+    document.getElementById('questionArea6').style.display = "none";
+}
+
+// --------- qst 7 ---------
+// next btn
+document.getElementById('nxtbut7').onclick = function(){
+    document.getElementById('questionArea8').style.display = "block"; 
+    document.getElementById('questionArea7').style.display = "none";
+}
+// prev btn
+document.getElementById('prevbut7').onclick = function(){
+    document.getElementById('questionArea6').style.display = "block";
+    document.getElementById('questionArea7').style.display = "none";
+}
+
+// --------- qst 8 ---------
+// next btn
+document.getElementById('nxtbut8').onclick = function(){
+    document.getElementById('questionArea9').style.display = "block"; 
+    document.getElementById('questionArea8').style.display = "none";
+}
+// prev btn
+document.getElementById('prevbut8').onclick = function(){
+    document.getElementById('questionArea7').style.display = "block";
+    document.getElementById('questionArea8').style.display = "none";
+}
+
+// --------- qst 9 ---------
+// next btn
+document.getElementById('nxtbut9').onclick = function(){
+    document.getElementById('questionArea10').style.display = "block"; 
+    document.getElementById('questionArea9').style.display = "none";
+}
+// prev btn
+document.getElementById('prevbut9').onclick = function(){
+    document.getElementById('questionArea8').style.display = "block";
+    document.getElementById('questionArea9').style.display = "none";
+}
+
+// --------- qst 10 ---------
+// prev btn
+document.getElementById('prevbut10').onclick = function(){
+    document.getElementById('questionArea9').style.display = "block"; 
+    document.getElementById('questionArea10').style.display = "none";
+}
+// submit btn
+document.getElementById('submit').onclick = function(){
+}
